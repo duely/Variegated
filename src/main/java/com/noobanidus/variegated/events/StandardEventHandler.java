f58513d631521ecef933cba219ca1c940469825e
Jon McManus <due@wxwhatever.com>
2019-06-29 21:03:03 +1000

    Defiled Ground recipe.

--- src/main/java/com/noobanidus/variegated/events/StandardEventHandler.java
+++ src/main/java/com/noobanidus/variegated/events/StandardEventHandler.java
@@ -3,24 +3,26 @@ package com.noobanidus.variegated.events;
 import com.noobanidus.variegated.entity.ai.EntityAILoveTempt;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.passive.EntityAnimal;
-import net.minecraft.entity.passive.EntityVillager;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber
+@SuppressWarnings("unused")
 public class StandardEventHandler {
   @SubscribeEvent
   public static void onEntityJoinWorld(EntityJoinWorldEvent event) {
     if (!(event.getEntity() instanceof EntityCreature)) {
       return;
     }
-    if (event.getEntity() instanceof EntityVillager) {
+    /*if (event.getEntity() instanceof EntityVillager) {
       ((EntityVillager) event.getEntity()).tasks.addTask(3, new EntityAILoveTempt((EntityVillager) event.getEntity(), 1.5D));
-    } else if (!event.getEntity().isCreatureType(EnumCreatureType.MONSTER, false) && event.getEntity() instanceof EntityAnimal) {
+    } else*/
+    if (!event.getEntity().isCreatureType(EnumCreatureType.MONSTER, false) && !(event.getEntity() instanceof IMob) && event.getEntity() instanceof EntityAnimal) {
       EntityAnimal animal = (EntityAnimal) event.getEntity();
-      animal.tasks.addTask(3, new EntityAILoveTempt(animal, 1.5D));
+      animal.tasks.addTask(3, new EntityAILoveTempt(animal, 1D));
     }
   }
 }
